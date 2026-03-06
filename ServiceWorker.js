57bd6b11b88c6a3273c36365fd8d3d07e5008351
Jake B <46630765+jakeb99@users.noreply.github.com>
2026-03-06 14:53:54 -0800

    v0.1.4

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "LawlantisDevTeam-Lawlantis-0.1.0";
 const contentToCache = [
-    "Build/7876ff803724b5efce7eb8cf10ae2561.loader.js",
+    "Build/de4c39fd18686187208b47d8c67d98cf.loader.js",
     "Build/5abc13a973568f463c6d3a3a5100e0bf.framework.js.unityweb",
-    "Build/e46a8808fd57befd5dee58c189126372.data.unityweb",
-    "Build/621d82e15b8880ffe3105514191ff815.wasm.unityweb",
+    "Build/606096bbcd83638e5b96856acd11bc02.data.unityweb",
+    "Build/3aeda1814a383a09083397b806e46b80.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
